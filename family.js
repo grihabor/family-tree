dbccf309c43f94a8f47322de1333357dd2738faf
grihabor <grihabor@mail.ru>
2017-04-15 16:09:53 +0300

    print data, move

--- family.js
+++ family.js
@@ -1,3 +1,19 @@
+function render_person(p){
+    body = $(".container");
+    
+}
+
 $.getJSON("data.json", function(json) {
-    $('#ta_json').html(json);
+    t = "";
+    for(var i in json){
+        person = json[i];
+        t += person.name + '<br>';
+    }
+
+    element = $('#ta_json');
+    
+    
+    element.offset({top: 100, left: 200});
+    
+    element.html(t);
 });
