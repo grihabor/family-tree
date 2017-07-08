8039ed824da6c1dffc96ee30628b42805126233b
grihabor <grihabor@mail.ru>
2017-07-08 16:18:10 +0300

    fixes

--- family.js
+++ family.js
@@ -160,15 +160,17 @@ function create_person_dict_and_couples(json) {
 
 }
 
-function render_line(pos, pos_to) {
-	pos = {
+function round(pos){
+ return {
 		x: Math.round(pos.x),
 		y: Math.round(pos.y)
 	}
-	pos_to = {
-		x: Math.round(pos_to.x),
-		y: Math.round(pos_to.y)
-	}
+}
+
+function render_line(pos, pos_to) {
+	pos = round(pos);
+	pos_to = round(pos_to);
+	
 	ctx.beginPath();
 	ctx.moveTo(pos.x, pos.y);
 	ctx.lineTo(pos_to.x, pos_to.y);
@@ -176,15 +178,10 @@ function render_line(pos, pos_to) {
 }
 
 function render_bezier(pos, pos_to) {
-	pos = {
-		x: Math.round(pos.x),
-		y: Math.round(pos.y)
-	}
-	pos_to = {
-		x: Math.round(pos_to.x),
-		y: Math.round(pos_to.y)
-	}
+	pos = round(pos);
+	pos_to = round(pos_to);
 	y_middle = Math.round((pos.y + pos_to.y) / 2);
+	
 	ctx.beginPath();
 	ctx.moveTo(pos.x, pos.y);
 	ctx.bezierCurveTo(pos.x, y_middle, pos_to.x, y_middle, pos_to.x, pos_to.y);
@@ -193,14 +190,8 @@ function render_bezier(pos, pos_to) {
 
 
 function render_zigzag(pos, pos_to, y_from) {
-	pos = {
-		x: Math.round(pos.x),
-		y: Math.round(pos.y)
-	};
-	pos_to = {
-		x: Math.round(pos_to.x),
-		y: Math.round(pos_to.y)
-	};
+	pos = round(pos);
+	pos_to = round(pos_to);
 	var y_center = Math.round((y_from + pos_to.y) / 2);
 	
 	ctx.beginPath();
@@ -219,10 +210,12 @@ function draw_connection(parents, child){
 	var parent_2 = person_dict[parents[1]];
 	child = person_dict[child];
 	
-	var parents_x = (
-		parent_1._x + parent_1.width / 2 +
-		parent_2._x + parent_2.width / 2
-	) / 2;
+ var parents_x = parent_1._x + parent_2._x;
+ if (parent_1._x < parent_2._x){
+  parents_x = (parents_x + parent_1.width) / 2;
+ } else {
+  parents_x = (parents_x + parent_2.width) / 2;
+ }
 	
 	var parents_y = parent_1._y + parent_1.height / 2;
      
