41b5b847a0f33fe507bffcd83136f2a9b4fb0964
grihabor <grihabor@mail.ru>
2017-07-20 19:15:33 +0300

    method neighbours

--- family.js
+++ family.js
@@ -1,4 +1,13 @@
 
+sigma.classes.graph.addMethod('neighbors', function(nodeId) { 
+    var k, 
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {}; 
+    for (k in index) 
+        neighbors[k] = this.nodesIndex[k]; 
+    return neighbors; 
+}); 
+
 sigma.parsers.json(
     'data/graph.json',
     {
